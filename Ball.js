0aa9b393dce7c74d04a1ab9a766e83434c808767
vanessanaja <vanessacnaja@gmail.com>
2019-05-10 15:56:47 -0700

    reset components to start from sscratch

--- Ball.js
+++ Ball.js
@@ -1,9 +1,1 @@
-import React, { Component } from "react";
-import "./Ball.css";
 
-class Ball extends Component {
-  render() {
-    return <div className='Ball'>{this.props.num}</div>;
-  }
-}
-export default Ball;
